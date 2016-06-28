dcc6165c282ab2604ae4bba80e148adb9422d53e
[lawrence_liu] <[1013269528@qq.com]>
2016-06-28 14:38:34 +0800

    添加了广告

--- zb/ad.php
+++ zb/ad.php
@@ -1,2 +1,4 @@
 ﻿<div style="padding:5px 15px; height:180px;"><img src="../gg.png" width="100%"></div>
+<br>
+<br>
 <script src="http://f.e719.net/cpf/index.php?76029_0|1|1__1_1000" charset="gb2312"></script>
